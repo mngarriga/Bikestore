e00c064a60c754503204858c515134ff2eae0065
Manuel Garriga <mngarriga@gmail.com>
2013-02-26 13:47:44 +0100

    Modidificar Category

--- BikeStore/src/main/java/bikestore/db/Category.java
+++ BikeStore/src/main/java/bikestore/db/Category.java
@@ -52,7 +52,7 @@ public class Category implements Serializable{
 
     @Override
     public String toString() {
-        return "Category{" + "name=" + name + '}';
+        return "name=" + name + '}';
     }
 
     //HasCode & Equals
