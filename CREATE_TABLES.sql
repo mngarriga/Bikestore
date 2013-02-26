46c4122ca26e937f29897e5b988c12e4d5e47f2c
DanKy <dlorengim@gmail.com>
2013-02-26 12:38:26 +0100

    Script creacion tablas B.D
    Script para la creacion de las tablas en la Base de Datos

---
+++ CREATE_TABLES.sql
@@ -0,0 +1,38 @@
+USE bikestore;
+DROP TABLE users;
+DROP TABLE articles;
+DROP TABLE categories;
+
+CREATE TABLE users (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	login		VARCHAR (20) NOT NULL,
+	passwd 		VARCHAR (40) NOT NULL,
+	fullName	VARCHAR (255) NOT NULL,
+	email		VARCHAR (255) NOT NULL,
+	address 	VARCHAR (255) NOT NULL,
+	city 		VARCHAR (255) NOT NULL,
+	country 	VARCHAR (255) NOT NULL,
+	zipcode 	MEDIUMINT UNSIGNED,
+	PRIMARY KEY (id),
+	UNIQUE KEY (login)
+);
+
+CREATE TABLE articles (
+	id 			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	name		VARCHAR (100) NOT NULL,
+	description VARCHAR (255) NOT NULL,
+	id_category	INT UNSIGNED NOT NULL,
+	price		DECIMAL(8,2) NOT NULL,
+	stock		INT NOT NULL,
+	picture		BLOB,
+	PRIMARY KEY (id),
+	UNIQUE KEY (name)	
+);
+
+CREATE TABLE categories (
+	id 			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	name		VARCHAR (100) NOT NULL,
+	id_cat_superior	INT UNSIGNED,	
+	PRIMARY KEY (id),
+	UNIQUE KEY (name)
+);
