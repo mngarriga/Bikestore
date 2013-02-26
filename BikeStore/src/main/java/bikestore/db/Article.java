bdf739deb301603afdd3c061ffa6b5b87ca94a80
DanKy <dlorengim@gmail.com>
2013-02-26 13:48:33 +0100

    Modificaciones Clase Article

--- BikeStore/src/main/java/bikestore/db/Article.java
+++ BikeStore/src/main/java/bikestore/db/Article.java
@@ -109,11 +109,36 @@ public class Article{
     public void setPicture(Byte[] picture) {
         this.picture = picture;
     }
+
     
+// toString, hashCode, equals --------------------------------------------------
     
-    
-    
-    
+    @Override
+    public String toString() {
+        return "Article{" + "name=" + name + ", price=" + price + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Article other = (Article) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }    
     
 } //class
 
