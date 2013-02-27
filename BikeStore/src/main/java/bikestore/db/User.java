269ed82cd99d60bdeb7cf8bed76840b873397036
Francisco Javier Martin Villuendas <fjmv28@hotmail.com>
2013-02-27 10:12:04 +0100

    clase User

--- BikeStore/src/main/java/bikestore/db/User.java
+++ BikeStore/src/main/java/bikestore/db/User.java
@@ -2,6 +2,10 @@ package bikestore.db;
 
 import java.io.*;
 import javax.persistence.*;
+import java.security.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Entity
 @Table(name="users")
@@ -20,7 +24,8 @@ public class User implements Serializable {
 
     public User(String login, String passwd, String fullName, String email, String adress, String city, String country, int zipcode) {
         this.login = login;
-        this.passwd = passwd;
+//        setPasswd(passwd);
+        this.passwd = getSha(passwd);
         this.fullName = fullName;
         this.email = email;
         this.adress = adress;
@@ -53,7 +58,18 @@ public class User implements Serializable {
     }
 
     public void setPasswd(String passwd) {
-        this.passwd = passwd;
+        this.passwd = getSha(passwd);
+    }
+    
+    private static String getSha(String s) {
+        try {
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            return new String(sha.digest(s.getBytes()));
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            return null;
+        }
     }
 
     public String getFullName() {
@@ -126,9 +142,10 @@ public class User implements Serializable {
         return true;
     }
 
-
+    //------------------
     // Active Record
-    
+    //------------------
+
     public boolean create(EntityManager em) {
         EntityTransaction et = em.getTransaction();
         try {
@@ -151,4 +168,87 @@ public class User implements Serializable {
         em.flush();
     }
     
+    public boolean remove(EntityManager em) {
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            boolean res = removeNoTransaction(em);
+            et.commit();
+            return res;
+        } catch (Exception e) {
+            if (et.isActive()) {
+                et.rollback();
+            }
+//            throw new Exception("Error saving user");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean removeNoTransaction(EntityManager em) {
+        if (em.find(User.class, this.getId()) != null) {
+            em.remove(this);
+            em.flush();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public static User findById(EntityManager em,long id) {
+        return em.find(User.class, id);
+    }
+    
+    public static List<User> findAll(EntityManager em) {
+        String sql = "SELECT u FROM User u ORDER BY u.id";
+        TypedQuery<User> q = em.createQuery(sql, User.class);
+        return q.getResultList();
+    }
+    
+    public static List<User> findByPage(EntityManager em,int page,int usersPerPage) {
+        String sql = "SELECT u FROM User u ORDER BY u.id";
+        TypedQuery<User> q = em.createQuery(sql, User.class);
+        q.setFirstResult((page - 1) * usersPerPage);
+        q.setMaxResults(usersPerPage);
+        return q.getResultList();
+    }
+    
+    
+    public static boolean exists(EntityManager em, String login) {
+        String sql = "SELECT u FROM User u WHERE u.login = :login";
+        TypedQuery<User> q = em.createQuery(sql,User.class);
+        q.setParameter("login", login);
+        try {
+            User user = q.getSingleResult();
+            return login.equals(user.getLogin());
+        } catch (Exception ex)
+        {
+            
+        }
+        finally {
+            return false;        
+        }
+    }
+    
+    public static User loginUser(EntityManager em, String login, String passwd) {
+        String sql = "SELECT u FROM User u WHERE u.login = :login";
+        TypedQuery<User> q = em.createQuery(sql,User.class);
+        q.setParameter("login", login);
+        try {
+            User user = q.getSingleResult();
+            
+            // check user
+            if (login.equals(user.getLogin())) {
+                // check passwd
+                if(user.getPasswd().equals(getSha(passwd))) {
+                    return user;
+                }
+            }
+            return null;
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return null;
+        }
+    }
 }
